881c064baa019f75f3302aa5667fcacedea1aecf
Thomas Maier <tmaier@fs.cs.hm.edu>
2015-03-19 18:20:31 +0100

    Support multiprocessing for three ZMap port scans.

--- hostscanner.sh
+++ hostscanner.sh
@@ -4,18 +4,21 @@ IP_RANGE=$1
 
 echo
 echo Starting ZMap with port 25
-echo =====================
-zmap -p 25 $IP_RANGE -b blacklist -o ../../results/zmap_output_25
+echo ==========================
+zmap -p 25 $IP_RANGE -b blacklist -o ../../results/zmap_output_25 &
 
 echo
 echo Starting ZMap with port 465
-echo ======================
-zmap -p 465 $IP_RANGE -b blacklist -o ../../results/zmap_output_465
+echo ===========================
+zmap -p 465 $IP_RANGE -b blacklist -o ../../results/zmap_output_465 &
 
 echo
 echo Starting ZMap with port 587
-echo ======================
-zmap -p 587 $IP_RANGE -b blacklist -o ../../results/zmap_output_587
+echo ===========================
+zmap -p 587 $IP_RANGE -b blacklist -o ../../results/zmap_output_587 &
+
+echo Wait for ZMap scans...
+wait
 
 echo
 echo DNS reverse lookup all hosts
